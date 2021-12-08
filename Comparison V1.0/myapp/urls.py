c2b77b61b0c825a755e4425e485ad44e12834835
moinnaik06 <52530558+moinnaik06@users.noreply.github.com>
2021-12-08 05:35:16 -0800

    Delete Comparison V1.0 directory

--- Comparison V1.0/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
- 	path('', views.chart, name='chart'),
- 	path('fetch/', views.fetch, name='fetch'),
- 	path('company/', views.fetch, name='company')	
- ]
