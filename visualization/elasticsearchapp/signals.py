a9586948abb7c6f0bb12d2a65c337a25606b0fb5
sabiha0613 <35830026+sabiha0613@users.noreply.github.com>
2021-12-08 05:43:58 -0800

    Delete visualization directory

--- visualization/elasticsearchapp/signals.py
+++
@@ -1,9 +0,0 @@
-from .models import Companies
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-
-@receiver(post_save, sender=Companies)
-def index_post(sender, instance, **kwargs):
-    print(instance)
-    instance.indexing()
--- visualization/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
- 	url(r'', views.fetch_compare, name='fetch'),
- 	url(r'fetch/', views.fetch_compare, name='fetch_compare'),
- 	url(r'company/', views.fetch_compare, name='company_compare')	
- ]
